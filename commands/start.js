546f1237e6c7fc95f4e83d2e2c06fdb49f023b7f
Ben <57199957+Leg3ndary@users.noreply.github.com>
2022-04-08 22:35:14 -0400

    Update some grammar?
    If you use contractions you should use them everywhere (can't, you will, you'll), also added a single newline to keep spacing consistent

--- commands/start.js
+++ commands/start.js
@@ -11,7 +11,7 @@ module.exports = {
         const embed = new MessageEmbed()
             .setTitle('Carl-Bot Help Desk')
             .setColor(0x5865F2)
-            .setDescription('Click the button below to start an interactive help desk.\nFrom there, you will be able to select a category and then find specific resources about it.\n\nCan’t find what you’re looking for? Ask a human in another support channel.\nSee <#805888259934257203>')
+            .setDescription('Click the button below to start an interactive help desk.\n\nFrom there, you\'ll be able to select a category and find specific resources about it.\n\nCan\'t find what you’re looking for? Ask a human in another support channel.\nSee <#805888259934257203>')
 
         const row = new MessageActionRow()
             .addComponents(

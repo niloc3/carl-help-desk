a3c526401f6ee12e3349394f2cfd8486aab88a02
Niloc <35661747+Niloc3@users.noreply.github.com>
2022-04-10 23:24:38 +0000

    ping edits msg rather then deletes and resends

--- commands/ping.js
+++ commands/ping.js
@@ -4,8 +4,7 @@ module.exports = {
 	execute(message, args, client) {
     if (!message.member.roles.cache.has(process.env.MODROLE)) return;
 		message.channel.send('Loading data').then (async (msg) =>{
-    msg.delete()
-    message.channel.send(`🏓Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
+    msg.edit(`🏓Latency is ${msg.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
   })
 	},
 };

d21d527947b197b89db16b68a82f9e18efec237a
Niloc <35661747+Niloc3@users.noreply.github.com>
2022-04-11 03:02:46 +0000

    analytics update

--- events/modalSubmit.js
+++ events/modalSubmit.js
@@ -4,7 +4,7 @@ const {sessions, sendFeedbackEmbed} = require('../helpers');
 
 module.exports = {
 	name: 'modalSubmit',
-	async execute(modal, client) {
+	async execute(modal, client, Discord, mixpanel) {
 		let id = modal.customId;
 		let msgid = id.split('-')[1];
 		let wasAnswerwed = id.split('-')[0] == 'Yes';
@@ -21,5 +21,8 @@ module.exports = {
 			components: [],
 			ephemeral: true,
 		});
+    mixpanel.track('Feedback Modal Submitted', {
+      distict_id: modal.user.id
+    })
 	},
 };
